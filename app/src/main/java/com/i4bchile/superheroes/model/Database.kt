6d9bb69ce795f916207a96980a43714f78194d5b
Jose Luis Ojeda <joseluisojedafdez@gmail.com>
2021-01-19 14:04:41 -0300

    Se añade el archivo Database con la interfaz de operaciones, la clase abstracta para la database y la aplicacion

--- app/src/main/java/com/i4bchile/superheroes/model/Database.kt
+++ app/src/main/java/com/i4bchile/superheroes/model/Database.kt
@@ -1,2 +1,38 @@
 package com.i4bchile.superheroes.model
 
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SuperDao{
+
+    @Insert
+    suspend fun loadAllSuper(heroList:List<SuperHero>)
+
+    @Query("SELECT * FROM superhero")
+    fun getAllSuper(): LiveData<List<SuperHero>>
+
+    @Query("SELECT * FROM superhero WHERE id=:superId")
+    fun getSuper(superId:Int)
+
+}
+
+@Database(entities=[SuperHero::class], version=1)
+abstract class SuperDatabase: RoomDatabase() {
+    abstract fun superDao():SuperDao
+}
+
+class SuperApplication: Application(){
+    companion object{
+        var superDatabase:SuperDatabase?=null
+
+    }
+
+    override fun onCreate(){
+        super.onCreate()
+
+        superDatabase=Room.databaseBuilder(this,SuperDatabase::class.java,"super_db").build()
+    }
+}
+
