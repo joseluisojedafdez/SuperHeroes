0f7c09667dd9be840b58ae68899ceab8430f7512
Jose Luis Ojeda <joseluisojedafdez@gmail.com>
2021-01-19 21:51:55 -0300

    se crea la funcionalidad en el ViewModel para obtener un heroe por id

--- app/src/main/java/com/i4bchile/heropedia/viewmodel/SuperHeroesVM.kt
+++ app/src/main/java/com/i4bchile/heropedia/viewmodel/SuperHeroesVM.kt
@@ -1,15 +1,27 @@
 package com.i4bchile.heropedia.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.i4bchile.heropedia.model.Repository
+import com.i4bchile.heropedia.model.SuperHero
 import kotlinx.coroutines.launch
 
 class SuperHeroesVM: ViewModel() {
 
-
     private val repository=Repository()
     val heroList=repository.superList
+
+
+   fun getHero(superId: Int): LiveData<SuperHero> {
+
+           return repository.getSuperHero(superId)
+
+
+   }
+
+
+
 
     init{
 
